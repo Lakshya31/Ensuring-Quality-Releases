85b13cb5bb99cbb46396ae8a3627f1b4612d975b
Lakshya31 <lakshyas317@gmail.com>
2022-07-10 00:33:32 +0530

    Added Changes to Logging Mechanism of Selenium

--- automatedtesting/selenium/ui_tests.py
+++ automatedtesting/selenium/ui_tests.py
@@ -1,5 +1,7 @@
 # #!/usr/bin/env python
+from asyncio.log import logger
 from datetime import datetime, timezone
+import logging
 import traceback
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,20 +9,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
+logging.basicConfig(filename="selenium_log.txt", level=logging.INFO)
 
 # Start the browser and login with standard_user
+
+
 def login(user, password):
     try:
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "),'Starting the browser...')
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + 'Starting the browser...')
         # --uncomment when running in Azure DevOps.
         options = ChromeOptions()
         options.add_argument("--headless")
         global driver
         driver = webdriver.Chrome(options=options)
         # driver = webdriver.Chrome()
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), 'Browser started successfully. Navigating to the demo page to login.')
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + 'Browser started successfully. Navigating to the demo page to login.')
         driver.get('https://www.saucedemo.com/')
 
         # Waiting for Page Load
@@ -30,30 +35,30 @@ def login(user, password):
         )
 
         # Entering Username
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), 'Finding Username Input Box')
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + 'Finding Username Input Box')
         username_input_box = driver.find_element(
             By.XPATH, "//input[@id='user-name']")
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), 'Entering Username in Input Box')
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + 'Entering Username in Input Box')
         username_input_box.send_keys(user)
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), 'Entered Username in Input Box!')
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + 'Entered Username in Input Box!')
 
         # Entering Password
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), 'Finding Password Input Box')
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + 'Finding Password Input Box')
         password_input_box = driver.find_element(
             By.XPATH, "//input[@id='password']")
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), 'Entering Password in Input Box')
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + 'Entering Password in Input Box')
         password_input_box.send_keys(password)
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), 'Entered Password in Input Box!')
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + 'Entered Password in Input Box!')
 
         # Click Login
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), 'Clicking on Login')
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + 'Clicking on Login')
         driver.find_element(By.XPATH, "//input[@id='login-button']").click()
 
         WebDriverWait(driver, 10).until(
@@ -61,10 +66,12 @@ def login(user, password):
                 (By.XPATH, "//span[text()='Products']"))
         )
 
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), "Successfully Logged in as user: {}!".format(user))
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + "Successfully Logged in as user: {}!".format(user))
 
     except Exception as err:
+        logging.error(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + "The program encountered an error")
         traceback.print_exc()
         raise err
 
@@ -77,25 +84,27 @@ def addtocart():
                 (By.XPATH, "//span[text()='Products']"))
         )
 
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), "Locating all Items")
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + "Locating all Items")
         itemNames = driver.find_elements(
             By.XPATH, "//*[@class='inventory_item_name']")
 
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), "Clicking on Add to cart for each item")
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + "Clicking on Add to cart for each item")
         for item in itemNames:
-            print(datetime.now(timezone.utc).strftime(
-                "%Y-%m-%d %H:%M:%S  "), "Clicking on Add to cart for item: {}".format(item.text))
+            logging.info(datetime.now(timezone.utc).strftime(
+                "%Y-%m-%d %H:%M:%S  ") + "Clicking on Add to cart for item: {}".format(item.text))
             addToCartElement = driver.find_element(
                 By.XPATH, "//*[text()='{}']/ancestor::*[3]//button[text()='Add to cart']".format(item.text))
             addToCartElement.click()
-            print(datetime.now(timezone.utc).strftime(
-                "%Y-%m-%d %H:%M:%S  "), "Successfully Clicked on Add to cart for item: {}".format(item.text))
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), "Successfully Added all Items to Cart!")
+            logging.info(datetime.now(timezone.utc).strftime(
+                "%Y-%m-%d %H:%M:%S  ") + "Successfully Clicked on Add to cart for item: {}".format(item.text))
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + "Successfully Added all Items to Cart!")
 
     except Exception as err:
+        logging.error(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + "The program encountered an error")
         traceback.print_exc()
         raise err
 
@@ -108,26 +117,28 @@ def removefromcart():
                 (By.XPATH, "//span[text()='Products']"))
         )
 
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), "Locating all Items")
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + "Locating all Items")
         itemNames = driver.find_elements(
             By.XPATH, "//*[@class='inventory_item_name']")
 
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), "Clicking on Remove from cart for each item")
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + "Clicking on Remove from cart for each item")
 
         for item in itemNames:
-            print(datetime.now(timezone.utc).strftime(
-                "%Y-%m-%d %H:%M:%S  "), "Clicking on Remove from cart for item: {}".format(item.text))
+            logging.info(datetime.now(timezone.utc).strftime(
+                "%Y-%m-%d %H:%M:%S  ") + "Clicking on Remove from cart for item: {}".format(item.text))
             addToCartElement = driver.find_element(
                 By.XPATH, "//*[text()='{}']/ancestor::*[3]//button[text()='Remove']".format(item.text))
             addToCartElement.click()
-            print(datetime.now(timezone.utc).strftime(
-                "%Y-%m-%d %H:%M:%S  "), "Successfully Clicked on Remove from cart for item: {}".format(item.text))
-        print(datetime.now(timezone.utc).strftime(
-            "%Y-%m-%d %H:%M:%S  "), "Successfully Removed all Items from Cart!")
+            logging.info(datetime.now(timezone.utc).strftime(
+                "%Y-%m-%d %H:%M:%S  ") + "Successfully Clicked on Remove from cart for item: {}".format(item.text))
+        logging.info(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + "Successfully Removed all Items from Cart!")
 
     except Exception as err:
+        logging.error(datetime.now(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S  ") + "The program encountered an error")
         traceback.print_exc()
         raise err
 
@@ -138,3 +149,5 @@ login('standard_user', 'secret_sauce')
 addtocart()
 removefromcart()
 driver.close()
+logging.info(datetime.now(timezone.utc).strftime(
+    "%Y-%m-%d %H:%M:%S  ") + "Execution Complete!")
